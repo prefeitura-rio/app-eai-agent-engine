38983dacc9d54b757517f3842daa7b0f5f88d6d2
d116626 <d116626@gmail.com>
2025-08-05 18:56:32 -0300

    chore: rool back

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -12,9 +12,6 @@ from vertexai.agent_engines import (
 )
 from src.agent.prompt import SYSTEM_PROMPT
 from src.agent.tools import mcp_tools
-from langgraph.checkpoint.postgres import PostgresSaver
-from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver
-from src.config import env
 
 
 class Agent(AsyncQueryable, AsyncStreamQueryable, Queryable, StreamQueryable):
@@ -25,24 +22,18 @@ class Agent(AsyncQueryable, AsyncStreamQueryable, Queryable, StreamQueryable):
         system_prompt: str = SYSTEM_PROMPT,
         tools: Sequence[Callable] = mcp_tools,
         temperature: float = 0.7,
-        pg_uri: str = env.PG_URI,
     ):
         self._model: str = model
         self._tools: Sequence[Callable] = tools
         self._graph: CompiledGraph = None
         self._system_prompt: str = system_prompt
         self._temperature: float = temperature
-        self._pg_uri: str = pg_uri
 
     def set_up(self):
         llm = ChatVertexAI(model_name=self._model, temperature=self._temperature)
         llm_with_tools = llm.bind_tools(tools=self._tools)
-
-        # Create AsyncPostgresSaver for checkpointing
         self._graph = create_react_agent(
-            llm_with_tools,
-            self._tools,
-            prompt=self._system_prompt,
+            llm_with_tools, self._tools, prompt=self._system_prompt
         )
 
     def query(self, **kwargs) -> dict[str, Any] | Any:
@@ -53,7 +44,6 @@ class Agent(AsyncQueryable, AsyncStreamQueryable, Queryable, StreamQueryable):
             yield dumpd(chunk)
 
     async def async_query(self, **kwargs) -> dict[str, Any] | Any:
-        print(kwargs)
         return await self._graph.ainvoke(**kwargs)
 
     async def async_stream_query(self, **kwargs) -> Iterator[dict[str, Any] | Any]:
